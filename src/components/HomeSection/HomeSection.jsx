91d373a7067cefa7dcc281e1fa10a519509c636d
nataliiacat <nataliagricaj6@gmail.com>
2025-01-15 11:42:13 -1100

    add bloc for section Home

--- src/components/HomeSection/HomeSection.jsx
+++ src/components/HomeSection/HomeSection.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './HomeSection.css'
 import virtual from '../../assets/images/virtua.jpg'
 import { IoIosArrowRoundForward } from 'react-icons/io'
+import { RiMapPinLine, RiMailLine } from 'react-icons/ri'
+import { BiPhoneCall } from 'react-icons/bi'
 
 const HomeSection = () => {
   return (
@@ -18,11 +20,35 @@ const HomeSection = () => {
                 nisl tincidunt eget. Lectus mauris eros in vitae.</p>
               <div className="btn-content flex">
                 <button className="btn">BUILD YOUR WORLD</button>
-                <IoIosArrowRoundForward className="icon hide-on-small" /></div>
+                <IoIosArrowRoundForward className="icon big-icon hide-on-small" /></div>
             </div>
             <div className="content-img">
               <img src={virtual} alt="virtual" />
             </div>
+          </div>
+          <div className="infoHome flex">
+            <div className="infoHome__item flex">
+              <RiMailLine className="icon big-icon" />
+              <div className="infoHome__info">
+                <h3 className="subtitle-info">Send Us a Message</h3>
+                <a href="contact@hydra.com">contact@hydra.com</a>
+              </div>
+            </div>
+            <div className="infoHome__item tel-info flex">
+              <BiPhoneCall className="icon big-icon" />
+              <div className="infoHome__info">
+                <h3 className="subtitle-info">Give Us a Call</h3>
+                <a href="tel:(110) 1111-1010" className="text">(110) 1111-1010</a>
+              </div>
+            </div>
+            <div className="infoHome__item flex">
+              <RiMapPinLine className="icon big-icon" />
+              <div className="infoHome__info">
+                <h3 className="subtitle-info">Pay Us a Visit</h3>
+                <address className="text">Union St, Seattle, WA 98101, United States</address>
+              </div>
+            </div>
+
           </div>
         </div>
       </section>
